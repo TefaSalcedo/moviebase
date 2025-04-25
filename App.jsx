155a1c1dff8bce5087c77e40b9cf3778d7ea8746
Tefa <stefa.21@hotmail.com>
2025-04-25 15:38:46 -0500

    Bases Movie List challenge

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function app() {
+
+    
+  return (
+    <div className="App">
+      <h1>Movie List</h1>
+      <bottom>Agregar nueva pelicula</bottom>
+      
+    
+    </div>
+  );
+}
+
+export default app;
